de22e8a5d5fe032acd8cf6d90fa18968c40e5dc3
mattscepter <chaudharyut25@gmail.com>
2021-09-16 22:37:20 +0530

    All carousel responsive

--- src/MyComponents/Partials/Header2.jsx
+++ src/MyComponents/Partials/Header2.jsx
@@ -1,43 +1,51 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header2 extends Component {
-
-  Header
+  Header;
 
   render() {
     return (
-      
-<<<<<<< HEAD
-      <div className="hidden lg:block px-4 sm:px-6 md:px-8 lg:px-12 2xl:px-0 mx-auto max-w-1366 text-sm -mb-16 font-for-para z-10">
-=======
       <div className="hidden lg:block px-4 sm:px-8 lg:px-12 2xl:px-0 mx-auto max-w-1366 text-sm -mb-16 font-for-para z-10">
->>>>>>> cbb13dcb985af1424793825f69a29c21bfdbbcaa
         <nav class="flex flex-wrap items-center text-base font-bold font-for-para">
-          <Link to="/about" className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60">
+          <Link
+            to="/about"
+            className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60"
+          >
             ABOUT
           </Link>
 
-          <Link to="/services" className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60">
-          SERVICES
+          <Link
+            to="/services"
+            className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60"
+          >
+            SERVICES
           </Link>
 
-          <Link to="/jobs" className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60">
-          JOBS
-          </Link>
-          
-          <Link to="/training" className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60">
-          COURSES
+          <Link
+            to="/jobs"
+            className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60"
+          >
+            JOBS
           </Link>
 
-          <Link to="/contact" className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60">
-          CONTACT
+          <Link
+            to="/training"
+            className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60"
+          >
+            COURSES
           </Link>
-      
+
+          <Link
+            to="/contact"
+            className="w-1/5 text-white py-4 text-center bg-black bg-opacity-40 hover:bg-opacity-60"
+          >
+            CONTACT
+          </Link>
         </nav>
       </div>
-    )
+    );
   }
 }
 
-export default Header2
+export default Header2;
