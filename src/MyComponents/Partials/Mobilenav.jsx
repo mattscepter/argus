c4101cf6d5de847df9d4240fbb75b047746244f7
Manas Shukla <shuklamanasofficial@gmail.com>
2021-09-18 18:43:21 +0530

    Mobile Nav

--- src/MyComponents/Partials/Mobilenav.jsx
+++ src/MyComponents/Partials/Mobilenav.jsx
@@ -3,76 +3,67 @@ import { Link } from "react-router-dom";
 
 const Mobilenav = ({ open }) => {
   return (
-    <div className={open ? "block" : "hidden"}>
-      <div
-        class={
-          open
-            ? "w-full h-screen bg-red-1 overflow-scroll "
-            : "w-full h-screen z-30 bg-red-1 overflow-scroll "
-        }
-      >
-        <div class="w-full">
-          <div class="w-full">
-            <div class="w-full flex flex-col mx-auto">
-              <Link to="/about" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  ABOUT
-                </button>
-              </Link>
+    <div className="z-50">
+      <div className="h-screen w-full flex flex-row">
+        <div className="sm:w-1/2 md:w-2/3 h-auto bg-red-1 bg-opacity-50">
 
-              <Link to="/services" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  SERVICES
-                </button>
-              </Link>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 bg-gray-3">
+          <nav className="list-none text-lg">
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+          </nav>
 
-              <Link to="/jobs" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  JOBS
-                </button>
-              </Link>
-
-              <Link to="/training" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  TRAINING
-                </button>
-              </Link>
-
-              <Link to="/contact" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  CONTACT
-                </button>
-              </Link>
-
-              <Link to="/technology" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  TECHNOLOGY
-                </button>
-              </Link>
-
-              <Link to="/training" class="w-11/12 md:w-7/12 lg:5/12 mx-auto">
-                <button class="w-full p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  GET YOUR SECURITY GUARD LICENSE
-                </button>
-              </Link>
-            </div>
-            <h1 class="text-white text-center text-3xl my-4 sm:my-3 ">
-              PORTALS
-            </h1>
-            <div class="w-full flex flex-row mx-auto">
-              <Link to="/login" class="w-1/4 mx-auto">
-                <button class="w-full my-1 p-4 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                  STUDENT PORTAL
-                </button>
-              </Link>
-              <button class="w-1/4 p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                CLIENT PORTAL
-              </button>
-              <button class="w-1/4 p-4 my-1 mx-auto rounded-lg border text-white bg-red-700 hover:bg-white hover:text-red-700 hover:border-red-700">
-                EMPLOYEE PORTAL
-              </button>
-            </div>
-          </div>
+          <nav className="list-none text-lg mt-8">
+            <li className="border-b-2 border-gray-1 text-white">
+              <div className="border-l-8 py-1.5 pl-3 border-opacity-0">
+                Portals
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+            <li className="border-b-2 border-gray-1 hover:border-white text-white">
+              <div className="border-l-8 py-1.5 pl-6 border-opacity-0 hover:border-opacity-100 hover:bg-black">
+                Services
+              </div>
+            </li>
+          </nav>
         </div>
       </div>
     </div>
